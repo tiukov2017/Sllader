b69f1ae61f76043b04031bdc6d809948756c7a23
Semion <semion.tiukov@gmailcom>
2018-07-06 23:21:43 +0300

    kommit 1

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+function Hello(){
+	return (
+		<h1>Hello World!</h1>)
+}
+
+ReactDOM.render(<Hello />, document.getElementById('root'));
+registerServiceWorker();
